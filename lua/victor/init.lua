a17dd853581c813ee43e5d9b7cb21ba149b79f93
Victor Johnsson <victor.johnsson@hotmail.com>
2024-04-20 12:47:30 +0200

    CSharp Dev works

--- lua/victor/init.lua
+++ lua/victor/init.lua
@@ -29,7 +29,7 @@ autocmd('TextYankPost', {
     end,
 })
 
-autocmd({"BufWritePre"}, {
+autocmd({ "BufWritePre" }, {
     group = ThePrimeagenGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
@@ -38,15 +38,20 @@ autocmd({"BufWritePre"}, {
 autocmd('LspAttach', {
     group = ThePrimeagenGroup,
     callback = function(e)
+        -- Check if the attached LSP client is OmniSharp
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() require('omnisharp_extended').lsp_definition() end, opts)
-
         vim.keymap.set("n", "D", function() require('omnisharp_extended').lsp_type_definition() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() require('omnisharp_extended').lsp_references() end, opts)
+        vim.keymap.set("n", "<leader>vri", function() require('omnisharp_extended').lsp_implementation() end, opts)
+
+
+        --vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() require('omnisharp_extended').lsp_references() end, opts)
+        --   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
