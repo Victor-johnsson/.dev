537dc2e6f2125cd678e1877a87dbfa98324e2e80
Victor Johnsson <victor.johnsson@hotmail.com>
2024-04-21 21:43:27 +0200

    noice, todo, fidget

---
+++ lua/victor/lazy/todo.lua
@@ -0,0 +1,18 @@
+return {
+
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+
+        vim.keymap.set("n", "<leader>td", function()
+            vim.cmd("TodoTelescope")
+        end)
+
+
+    }
+}
