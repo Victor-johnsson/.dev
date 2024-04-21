f5bbc2979df3f22adbb1fcf2185d243678fc8abb
Victor Johnsson <victor.johnsson@hotmail.com>
2024-04-21 20:24:30 +0200

    new colorscheme

--- lua/victor/lazy/colors.lua
+++ lua/victor/lazy/colors.lua
@@ -1,51 +1,43 @@
-function ColorMyPencils(color)
-    color = color or "rose-pine"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true,     -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark",   -- style for floating windows
-                },
-            })
-        end
-    },
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
 
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
+    config = function()
+        require("solarized-osaka").setup({
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            transparent = true, -- Enable this to disable setting the background color
+            terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+            styles = {
+                -- Style to be applied to different syntax groups
+                -- Value is any valid attr-list value for `:help nvim_set_hl`
+                comments = { italic = true },
+                keywords = { italic = true },
+                functions = {},
+                variables = {},
+                -- Background styles. Can be "dark", "transparent" or "normal"
+                sidebars = "dark",  -- style for sidebars, see below
+                floats = "dark",    -- style for floating windows
+            },
+            sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+            day_brightness = 0.3,   -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+            hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+            dim_inactive = false,   -- dims inactive windows
+            lualine_bold = false,   -- When `true`, section headers in the lualine theme will be bold
 
-            vim.cmd("colorscheme rose-pine")
+            --- You can override specific color groups to use other groups or a hex color
+            --- function will be called with a ColorScheme table
+            ---@param colors ColorScheme
+            on_colors = function(colors) end,
 
-            ColorMyPencils()
-        end
-    },
-
-
+            --- You can override specific highlights to use other groups or a hex color
+            --- function will be called with a Highlights and ColorScheme table
+            ---@param highlights Highlights
+            ---@param colors ColorScheme
+            on_highlights = function(highlights, colors) end,
+        })
+        vim.cmd("colorscheme solarized-osaka")
+    end
 }
-
