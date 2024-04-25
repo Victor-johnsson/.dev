32b3aefdd3fec3144e56d965e75ead479f7bf687
Victor Johnsson <victor.johnsson@capgemini.com>
2024-04-25 13:58:23 +0200

    ToggleTerm keymaps

--- lua/victor/lazy/toggle-term.lua
+++ lua/victor/lazy/toggle-term.lua
@@ -6,5 +6,8 @@ return {
     version = "*",
     config = function()
         require("toggleterm").setup({})
+
+        vim.keymap.set("n", "<leader>tg", '<cmd>ToggleTerm<cr>', {})
+        vim.keymap.set("n", "<leader>tg", '<cmd>ToggleTerm<cr>', {})
     end
 }
--- lua/victor/remap.lua
+++ lua/victor/remap.lua
@@ -36,3 +36,4 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+vim.keymap.set("t", '<Esc>', '<C-\\><C-n>', { noremap = true })
