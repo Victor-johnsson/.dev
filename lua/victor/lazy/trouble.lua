960ce899a35bf0cf625f803f012e75c6315d8edb
Victor Johnsson <victor.johnsson@capgemini.com>
2025-02-06 19:14:11 +0100

    Only show errors with trouble!

--- lua/victor/lazy/trouble.lua
+++ lua/victor/lazy/trouble.lua
@@ -1,11 +1,12 @@
 return {
     "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {
+    }, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
     keys = {
         {
             "<leader>xx",
-            "<cmd>Trouble diagnostics toggle<cr>",
+            "<cmd>Trouble diagnostics filter = { severity=vim.diagnostic.severity.ERROR} <cr>",
             desc = "Diagnostics (Trouble)",
         },
         {
